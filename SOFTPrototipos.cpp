2639240ca7a6f4ba417bbd42b08d933f3aa94458
gerecorrea <48485066+gerecorrea@users.noreply.github.com>
2019-05-07 21:32:39 -0300

    Add files via upload

---
+++ SOFTPrototipos.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <cstdlib>
+#include <clocale>
+
+using namespace std;
+
+
+int main() {
+	/*
+	//Protótitpo pro menu
+    cout << endl;
+	cout << "************** SIRU **************" << endl;
+    cout << "---------- MENU INICIAL-----------" << endl;
+    cout << "Já e cliente?" << endl;
+    cout << "[1] - Não, desejo me cadastrar." << endl;
+    cout << "[2] - Sim, sou. Desejo me logar." << endl;
+	cout << "[0] - Encerrar aplicativo." << endl; 
+	cout << "----------------------------------" << endl;
+	*/
+
+	/*
+	//Prototipo pro cadastro de cliente - 1.1:
+	cout << endl;
+	cout << "************** SIRU **************" << endl;
+    cout << "------------ CADASTRO ------------" << endl;
+    cout << "Digite seus dados a seguir:" << endl<< endl;
+    cout << "Codigo de registro: 123456789" << endl<< endl;//gerado por rand aleatoriamente (n pode repetir)
+    cout << "Nome: " << endl << endl;
+	cout << "Nome de login: " << endl << endl;
+	cout << "Senha de login: " << endl << endl;
+    cout << "E-mail: " << endl << endl;
+	cout << "Numero de celular" << endl << endl;
+	cout << "----------------------------------" << endl; 
+	*/
+
+	/*
+	//Protótipo de login do cliente - 1.2:
+	cout << endl;
+	cout << "************** SIRU **************" << endl;
+    cout << "------------- LOGIN --------------" << endl;
+    cout << "Login: " << endl << endl;
+	cout << "Senha: " << endl << endl;
+    cout << "----------------------------------" << endl; 
+	*/
+
+	/*
+	//Protótipo de menu do cliente - após login correto no 1.2:
+	cout << endl <<  "Usuario: Frodo123 \t\t\t Caixa de mensangens: 4" << endl << endl;
+	cout << "[1] - Adicionar ruas às marcadas para receber notificações." << endl;
+    cout << "[2] - Adicionar veículos aos seus veículos escolhidos." << endl;//mostra os veículos aceitos e pede que você selecione um, voltando ao menu do clinte.
+    cout << "[3] - Ver ruas que está apto a receber notificações e estados atuais." << endl;//apresenta ela e as condições delas
+	cout << "[4] - Ver seus veículos adicionados." << endl;
+    cout << "[5] - Ver caixa de mensagens." << endl;
+	cout << "[6] - Sair da conta. " << endl; //volta ao menu inicial do aplicativo
+	*/
+
+	/*
+	//Protótipo de adição de ruas às favoritas - 1.2.1:
+	//Rua, Bairro (o que divide a rua em partições)
+	cout << endl;
+	cout << "************** SIRU **************" << endl;
+    cout << "---- ADIÇÃO DE RUAS FAVORITAS ----" << endl;
+	cout << "[1] - Aaron da Silva, Saguaçu." << endl;
+    cout << "[2] - Enzo Valentino, Centro." << endl;
+    cout << "[3] - João das Neves, Westeros." << endl;
+	cout << "[4] - João das Neves, Essos." << endl;
+    cout << "[5] - São Paulo, Centro." << endl; 
+	cout << "[0] - Voltar ao menu do usuário." << endl;	
+	cout << "----------------------------------" << endl; 
+	*/
+
+	/*
+	//Protótipo de adição de carros - 1.2.2:
+	cout << endl;
+	cout << "************** SIRU **************" << endl;
+    cout << "-------- ADIÇÃO DE CARROS --------" << endl;
+	cout << "[1] - Bicicleta" << endl;
+    cout << "[2] - Carro." << endl;
+    cout << "[3] - Moto." << endl;
+	cout << "[4] - Caminhão/Onibus." << endl; 
+	cout << "[0] - Voltar ao menu do usuário." << endl;	
+	cout << "----------------------------------" << endl; 
+	*/
+
+	/*
+	//Protótipo para ver todas as ruas e seus estados - 1.2.3.
+	cout << endl;
+	cout << "************** SIRU **************" << endl;
+    cout << "-------- SITUAÇÂO DAS RUAS -------" << endl;
+	cout << "Rua: João das Neves, Westeros." << endl;
+    cout << "Situação: Muito transito." << endl << endl;
+    cout << "Rua: João das Neves, Essos." << endl;
+	cout << "Situação: Normal." << endl << endl; 
+	cout << "Rua: São Paulo, Centro. " << endl;	
+	cout << "----------------------------------" << endl; 
+	*/
+
+	/*
+  //Protótipo para ver a caixa de mensagens - 1.2.5
+  cout << endl;
+	cout << "************** SIRU **************" << endl;
+  cout << "------- CAIXA DE MENSAGENS -------" << endl;
+	cout << "Aviso: A rua João das Neves, Westeros foi interditada de forma total esta manhã." << endl;
+  cout << "Situação: Acidente grave com interdição total." << endl;
+  cout << "Data: 08/05/2019. Hora: 08:46" << endl << endl;
+	cout << "Aviso: A rua São Paulo está com congestionamento." << endl;
+	cout << "Situação: trânsito levemente acima do normal" << endl;
+  cout << "Data: 08/05/2019. Hora: 07:49" << endl;
+	cout << "----------------------------------" << endl;
+  */
+
+}
